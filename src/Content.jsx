ac6e323d4599ec202f574ef0d671f10084d7c081
JRisJunior <jeffmrubin@gmail.com>
2023-01-24 12:34:22 -0600

    add react player to app and website

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,8 +1,12 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
 export function Content() {
   return (
     <div>
       <h1>Welcome to React!</h1>
       <p>this is a test</p>
+      <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' controls={true} />
     </div>
   );
 }
